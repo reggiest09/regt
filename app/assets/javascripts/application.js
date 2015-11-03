6a6db0d3d98d9dbbe6dfee17033ea7efc6ba43ca
Reggie Tucker <reggiest09@gmail.com>
2015-11-03 14:37:57 -0500

    proj

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,34 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require jquery-1.10.2.min
+//= require bootstrap
+//= require jquery.superslides
+//= require plugins
+//= require jquery.appear
+//= require waypoints.min
+//= require jquery.prettyPhoto
+//= require modernizr-latest
+//= require SmoothScroll
+//= require jquery.parallax-1.1.3
+//= require jquery.easing.1.3
+//= require jquery.flexslider
+//= require jquery.sticky
+//= require owl.carousel
+//= require jquery.isotope
+//= require rev-slider/jquery.themepunch.plugins.min
+//= require rev-slider/jquery.themepunch.revolution.min
+//= require jquery.mb.YTPlayer
+//= require jquery.fitvids
+//= require_tree .
